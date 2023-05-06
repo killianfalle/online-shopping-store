948eb588a653d42a6f991f3469e6ac0f7b2cf488
killianfalle <killianfalle7@gmail.com>
2023-05-06 11:30:49 +0800

    Implement filter and search product

--- src/components/products.tsx
+++ src/components/products.tsx
@@ -1,18 +1,42 @@
-import { FC, useContext } from 'react';
+import { FC, useContext, useEffect, useState } from 'react';
 import { Product } from '../types/product';
 import { Context } from '../context/context';
 import "../assets/product.css"
 
 const Products: FC<ProductsProps> = ({ products }) => {
+  const [searchTerm, setSearchTerm] = useState('');
+  const [filteredProducts, setFilteredProducts] = useState<Product[]>([]);
   const {addToCart} = useContext(Context)
+  const productsToShow = searchTerm ? filteredProducts : products;
+
+  const handleSearch = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setSearchTerm(event.target.value);
+  };
 
   const handleAddToCart = (item: Product) => {
     addToCart(item)
   }
 
+  useEffect(() => {
+    const filtered = products.filter(product =>
+      product.productName.toLowerCase().includes(searchTerm.toLowerCase()) ||
+      product.category.toLowerCase().includes(searchTerm.toLowerCase())
+    );
+
+    setFilteredProducts(filtered);
+  }, [searchTerm])
+
   return (
     <div className="products-container">
-      {products.map((item, index) => (
+      <input
+        type="text"
+        placeholder="Search product by name or category"
+        className="search-input"
+        value={searchTerm}
+        onChange={handleSearch}
+      />
+
+      {productsToShow.map((item, index) => (
         <div
           key={index}
           className="product-item-container">
